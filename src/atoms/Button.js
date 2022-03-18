299762735fa0e57426fc7e283a2c378b447a7944
sachin <sachindraragul@gmail.com>
2022-03-18 22:11:06 +0530

    useRef initial commit

--- src/atoms/Button.js
+++ src/atoms/Button.js
@@ -4,9 +4,30 @@ export const ButtonWithForwardRef = forwardRef((props, ref) => {
   useEffect(() => {
     console.log(ref.current);
   }, []);
-  return <button ref={ref}>Hey . I'm Button with Forward ref</button>;
+
+  const handleBtnClick = () => {
+    console.log(ref.current);
+  };
+
+  return (
+    <button onClick={handleBtnClick} ref={ref}>
+      Hey . I'm Button with forward ref
+    </button>
+  );
 });
 
-export const ButtonWithoutForwardRef = (props, ref) => {
-  return <button ref={props.ref}>Hey. I'm with without forward ref</button>;
+// You cannot access ref inside a child component like this. It will throw warning
+export const ButtonWithoutForwardRef = (props) => {
+  useEffect(() => {
+    console.log(props.ref?.current); // This will be undefined
+  }, []);
+
+  const handleBtnClick = () => {
+    console.log(props.ref?.current); // undefined
+  };
+  return (
+    <button onClick={handleBtnClick} ref={props.ref}>
+      Hey. I'm with without forward ref
+    </button>
+  );
 };
